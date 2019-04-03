77b5dd3db8c88adc2e8fccddd080f44bbdb790dc
Hunter Rushton <hunterrushton@gmail.com>
2019-04-03 05:00:38 -0600

    kys kiddo

---
+++ src/APP.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+
+public class APP {
+
+    int cha = 10;
+    int dex = 10;
+    int str = 10;
+    int wis = 10;
+    int con = 10;
+    int igt = 10;
+
+    int stats[] = {cha, dex, str, wis, con, igt};
+
+    public void human(){
+
+
+    }
+
+    public static void main(String[] args){
+        ;
+    }
+}
---
+++ src/Race.java
@@ -0,0 +1,11 @@
+public class Race {
+
+    String[] race = {"Human", "Dragonborn", "Dwarf", "Elf", "Gnome", "Halfling", "Half-Elf", "Half-Orc", "Tiefling",
+    "Aarakocra", "Genasi", "Goliath", "Aasimar", "Bugbear", "Firbolg", "Goblin", "Hobgoblin", "Kenku", "Kobold",
+    "Lizardfolk", "Orc", "Tabaxi", "Triton", "Yuan-ti Pureblood", "Feral Tiefling", "Tortle", "Gith", "Changeling",
+    "Kalashtar", "Shifter", "Warforged", "Centaur", "Loxodon", "Minotaur", "Simic Hybrid", "Vedalken"};
+
+    public void Human(){
+        
+    }
+}
